508a9ce0ce3988f9b26832c45087dd7175971380
shlykov <shlykov@BATAEV.imc-dmn.parus-s.ru>
2017-07-06 17:06:56 +0400

    Merge origin/master
    Conflicts:
    	src/main/java/com/mycompany/testproject/FXMLController.java
    	src/main/resources/fxml/Scene.fxml

--- src/main/java/com/mycompany/testproject/FXMLController.java
+++ src/main/java/com/mycompany/testproject/FXMLController.java
@@ -2,8 +2,13 @@ package com.mycompany.testproject;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
 
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,6 +17,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -25,10 +33,10 @@ public class FXMLController implements Initializable {
     private Pane pane;
     @FXML
     private Pane mypane;
+    @FXML
+    private Spinner spinner;
     
-    @FXML
     private Double oldX;
-    @FXML
     private Double oldY;
    
     
@@ -54,8 +62,46 @@ public class FXMLController implements Initializable {
         stackPane.getChildren().add(root);
     }
     
+    private void test(){
+        //Spinner<LocalDate> s = new Spinner<>();
+        
+        ObservableList<LocalDate> list; //todo: сделать инкапсулированно
+        LocalDate day1 = LocalDate.now();
+        LocalDate day2 = LocalDate.of(2010, Month.MARCH, 20);
+        list = FXCollections.observableArrayList(day1,day2);
+//        spinner = new Spinner<LocalDate>();
+        SpinnerValueFactory<LocalDate> valueFactory = new SpinnerValueFactory<LocalDate>() {
+            @Override
+            public void decrement(int steps) {
+                LocalDate current = this.getValue();
+                int idx = list.indexOf(current);
+                int newIdx = (list.size()+idx-steps)%list.size();
+                LocalDate newLang = list.get(newIdx);
+                this.setValue(newLang);
+            }
+
+            @Override
+            public void increment(int steps) {
+                LocalDate current = this.getValue();
+                int idx = list.indexOf(current);
+                int newIdx = (idx+steps)%list.size();
+                LocalDate newLang = list.get(newIdx);
+                this.setValue(newLang);
+            }
+        };
+        valueFactory.setValue(day1);
+        spinner.setValueFactory(valueFactory);
+
+        
+        /*spinner.setOnMouseClicked(e->{spinner.setVisible(false);});
+        SpinnerValueFactory<LocalDate> svf = new SpinnerValueFactory.ListSpinnerValueFactory<>(list);
+        svf.setValue(list.get(0));
+        spinner.setValueFactory(svf);*/
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        test();
     }    
 }
